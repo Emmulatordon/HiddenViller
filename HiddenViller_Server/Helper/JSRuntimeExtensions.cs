601c29e4b27e5f65108d54f5e7780d8f5fa3abf8
Emmulatordon <emmulatordon@gmail.com>
2023-08-24 20:49:27 +0100

    Added toastr and sweet alerts to project

--- HiddenViller_Server/Helper/JSRuntimeExtensions.cs
+++ HiddenViller_Server/Helper/JSRuntimeExtensions.cs
@@ -9,9 +9,22 @@ namespace HiddenViller_Server.Helper
         {
             await Runtime.InvokeVoidAsync("toastr.success", message, title);
         }
+        public static async ValueTask ToastrError(this IJSRuntime Runtime, string message, string title)
+        {
+            await Runtime.InvokeVoidAsync("toastr.error", message, title);
+        }
+        public static async ValueTask ToastrWarning(this IJSRuntime Runtime, string message, string title)
+        {
+            await Runtime.InvokeVoidAsync("toastr.warning", message, title);
+        }
+
         public static async ValueTask SweetAlertSuccess(this IJSRuntime Runtime,string heading, string message)
         {
             await Runtime.InvokeVoidAsync("Swal.fire", heading, message, "success");
+        }
+        public static async ValueTask SweetAlertError(this IJSRuntime Runtime, string heading, string message)
+        {
+            await Runtime.InvokeVoidAsync("Swal.fire", heading, message, "error");
         }
     }
 }
