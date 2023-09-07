58decc73ae4a25fb2d5fd578aab6a32bd93512b1
Emmulatordon <emmulatordon@gmail.com>
2023-09-07 08:16:25 +0100

    AddedApiProjectAndExtendedIdentityUserWithANameProperty

---
+++ HiddenVilla_Api/Controllers/HotelRoomController.cs
@@ -0,0 +1,55 @@
+﻿using Business.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System;
+using System.Threading.Tasks;
+using Models;
+
+namespace HiddenVilla_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelRoomController : ControllerBase
+    {
+        private readonly IHotelRoomRepository _hotelRoomRepository;
+
+        public HotelRoomController(IHotelRoomRepository hotelRoomRepository)
+        {
+            _hotelRoomRepository = hotelRoomRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetHotelRooms()
+        {
+            var allRooms = await _hotelRoomRepository.GetAllHotelRooms();
+            return Ok(allRooms);
+        }
+
+        [HttpGet("{roomId}")]
+        public async Task<IActionResult> GetHotelRoom(int? roomId)
+        {
+            if (roomId == null)
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    Title = "",
+                    ErrorMessage = "Invalid Room Id",
+                    StatusCode = StatusCodes.Status400BadRequest
+                });
+            }
+            
+            var roomDetails = await _hotelRoomRepository.GetHotelRoom(roomId.Value);
+            if (roomDetails == null)
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    Title = "",
+                    ErrorMessage = "Invalid Room Id",
+                    StatusCode = StatusCodes.Status404NotFound
+                });
+            }
+            return Ok(roomDetails);
+        }
+    }
+}
